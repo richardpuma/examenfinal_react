247b75025eae14e9e954868355bbdee15ce51c40
richard pumapillo <richardpumac@gmail.com>
2019-12-07 09:51:34 -0500

    se agrego el login

--- src/components/Pages/Login.jsx
+++ src/components/Pages/Login.jsx
@@ -8,12 +8,31 @@ import Col from 'react-bootstrap/Col'
 const Login = () => (
   <Container>
     <Row className="justify-content-md-center">
-      <h1>HOME</h1>      
+      <h1>Iniciar Sesión</h1>      
     </Row>
     <Row className="justify-content-md-center">
       <Col xs lg="2"></Col>
       <Col md="auto">
-       
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" required />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" required />
+          </Form.Group>
+          <Form.Group controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Iniciar Sessión
+          </Button>
+        </Form>
       </Col>
       <Col xs lg="2"></Col>
     </Row>
