226f152fd6c22334e86c716785d1d5787e0ef393
YeseniaBelen <yesenia.chiroque@outlook.com>
2019-12-07 10:20:34 -0500

    view1

--- src/components/Organisms/Header.jsx
+++ src/components/Organisms/Header.jsx
@@ -23,7 +23,7 @@ const Header = () => (
     <Nav>
       <Nav.Link href="#deets">More deets</Nav.Link>
       <Nav.Link eventKey={2} href="#memes">
-        Dank memes
+        
       </Nav.Link>
     </Nav>
   </Navbar.Collapse>
