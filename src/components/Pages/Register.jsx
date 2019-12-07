bcd567a27aead84e6c8749bd8d539cfea0febb79
YeseniaBelen <yesenia.chiroque@outlook.com>
2019-12-07 10:42:55 -0500

    create acount

--- src/components/Pages/Register.jsx
+++ src/components/Pages/Register.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 const Register = () => (
     <div>
-      <h1>Register</h1>
+      <h1>Create acount</h1>
+    <Form>
+      <Form.Group controlId="formBasicEmail">
+        <Form.Label>Acount</Form.Label>
+        <Form.Control type="email" placeholder="Acount" />
+        <Form.Text className="text-muted">
+        
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group controlId="formBasicPassword">
+        <Form.Label>Balance</Form.Label>
+        <Form.Control type="password" placeholder="Balance" />
+      </Form.Group>
+    
+      <Button variant="primary" type="submit">
+        Register
+      </Button>
+    </Form>
+
     </div>
+    
+
   )
 
 export default Register
