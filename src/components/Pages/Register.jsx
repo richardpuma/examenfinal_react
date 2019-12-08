b48e5188b3bca1568d77d88fcead28c8700d6587
YeseniaBelen <yesenia.chiroque@outlook.com>
2019-12-07 23:53:44 -0500

    create acount form

--- src/components/Pages/Register.jsx
+++ src/components/Pages/Register.jsx
@@ -1,33 +1,38 @@
 import React from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 const Register = () => (
-    <div>
-      <h1>Create acount</h1>
-      
-    <Form>
-      <Form.Group controlId="formBasicEmail">
-        <Form.Label>Acount</Form.Label>
-        <Form.Control type="email" placeholder="Acount" />
-        <Form.Text className="text-muted">
-        
-        </Form.Text>
-      </Form.Group>
-
-      <Form.Group controlId="formBasicPassword">
-        <Form.Label>Balance</Form.Label>
-        <Form.Control type="password" placeholder="Balance" />
-      </Form.Group>
+  <Container>
     
-      <Button variant="primary" type="submit">
-        Register
-      </Button>
-    </Form>
+   <Row className="justify-content-md-left">
 
-    </div>
-    
+    <Col xs lg="6">
+      <Col md="auto">
+        <br></br>
+      <h3>Create acount</h3>
+          <Form>
+            <Form.Group controlId="formBasicAcount">
+              <Form.Label>Acount</Form.Label>
+              <Form.Control type="text" placeholder="Number acount" required/>
+            </Form.Group>
 
+            <Form.Group controlId="formBasicBalance">
+              <Form.Label>Balance</Form.Label>
+              <Form.Control type="numeric" value="0.00" required/>
+            </Form.Group>
+          
+            <Button variant="primary" type="submit">
+              Register
+            </Button>
+          </Form>
+       </Col>
+     </Col>
+    </Row>
+    </Container>
   )
 
 export default Register
